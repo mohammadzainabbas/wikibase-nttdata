501f39054801861d6c4836cc9ce95effc640effe
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-04 22:20:49 +0000

    Updated code

--- src/create_all_mapping.py
+++ src/create_all_mapping.py
@@ -81,9 +81,9 @@ def main() -> None:
     wbi = WikibaseIntegrator(login=login)
 
     mapping_files = ['team_member_mapping.csv', 'project_mapping.csv', 'organization_mapping.csv', 'task_mapping.csv']
-
+    __mapping
     for file in mapping_files:
-        project_mapping = join(data_dir, "project_mapping.csv")
+        project_mapping = join(data_dir, file)
 
 
 
