104ac56490405adab287b8413cba502fbe1c2614
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-04 23:53:01 +0000

    Minor changes

--- src/create_projects.py
+++ src/create_projects.py
@@ -140,9 +140,9 @@ def main() -> None:
     df = pd.read_csv(column_mapping_data)
     column_mapping = df.set_index("column").T.to_dict()
 
-    __columns = ["Project", "Project Type", "Client"]
+    __columns = ["Project", "Project Type", "Client", "Owner"]
     __df = pd.read_csv(project_data, usecols=__columns).drop_duplicates()
-    __data = __df.set_index("Client").T.to_dict()
+    __data = __df.set_index("Project").T.to_dict()
 
     __res = list()
     if exists(items_data):
