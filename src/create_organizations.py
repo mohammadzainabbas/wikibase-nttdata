b218370c62b45c068db2e458f6a7f3479bf1c768
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-04 23:36:55 +0000

    Minor update

--- src/create_organizations.py
+++ src/create_organizations.py
@@ -106,7 +106,7 @@ def create_organization(data: dict, mapping: list, wbi: WikibaseIntegrator) -> d
 
     try:
         res = item.write()
-        ident = [x for x in str(res).split('\n') if "_id='P" in x]
+        ident = [x for x in str(res).split('\n') if "_id='Q" in x]
         if len(ident) == 1:
             prop_code = ident[0].split("'")[1]
             __res["prop"] = prop_code
