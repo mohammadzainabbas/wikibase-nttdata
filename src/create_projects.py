79b163c712fa830e2bf100ab0829658a3c2141bd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-04 23:52:40 +0000

    Updated code

--- src/create_projects.py
+++ src/create_projects.py
@@ -140,7 +140,7 @@ def main() -> None:
     df = pd.read_csv(column_mapping_data)
     column_mapping = df.set_index("column").T.to_dict()
 
-    __columns = ["Client Type", "Client"]
+    __columns = ["Project", "Project Type", "Client"]
     __df = pd.read_csv(project_data, usecols=__columns).drop_duplicates()
     __data = __df.set_index("Client").T.to_dict()
 
