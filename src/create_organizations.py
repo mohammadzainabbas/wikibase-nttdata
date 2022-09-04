390bbc87d37ad4c4e35b686a029843930d58b1af
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-04 22:38:16 +0000

    Minor update

--- src/create_organizations.py
+++ src/create_organizations.py
@@ -99,6 +99,10 @@ def main() -> None:
     wbi = WikibaseIntegrator(login=login)
 
     __columns = ["Client Type", "Client"]
+    __df = pd.read_csv(project_data, usecols=__columns)
+
+    
+
 
     mapping_files = ['team_member_mapping.csv', 'project_mapping.csv', 'organization_mapping.csv', 'task_mapping.csv']
     __mapping = list()
