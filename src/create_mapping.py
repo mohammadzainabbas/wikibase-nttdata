686ed7b1531d503981a79bf11b3e19d8a5a0c981
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-04 16:49:56 +0000

    Work in progress

--- src/create_mapping.py
+++ src/create_mapping.py
@@ -1,2 +1,16 @@
 #! /root/anaconda3/envs/wikibase/bin/python
 
+from wikibaseintegrator.models import Qualifiers, References, Reference
+from wikibaseintegrator import WikibaseIntegrator
+from wikibaseintegrator import wbi_login
+from wikibaseintegrator import datatypes
+from wikibaseintegrator.wbi_config import config as wbi_config
+from os.path import join, isfile, exists, isdir, abspath, pardir
+from os import listdir, getcwd
+import pandas as pd
+from wikibaseintegrator.datatypes import (URL, CommonsMedia, ExternalID, Form, GeoShape, GlobeCoordinate, Item, Lexeme, Math, MonolingualText, MusicalNotation, Property, Quantity, Sense, String, TabularData, Time)
+from wikibaseintegrator.wbi_enums import ActionIfExists, WikibaseDatePrecision, WikibaseRank, WikibaseSnakType
+from wikibaseintegrator.wbi_exceptions import MissingEntityException, ModificationFailed, MWApiError
+
+if __name__ == "__main__":
+    main()
