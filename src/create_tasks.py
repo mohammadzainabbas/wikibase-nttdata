f59809083bd3dad718277169ab852e56264c2063
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-05 06:15:03 +0000

    Updated code

--- src/create_tasks.py
+++ src/create_tasks.py
@@ -53,10 +53,9 @@ def convert_to_wikibase_datatype(__value, __propcode, __data_type, __res, wbi):
     elif __data_type == WikibaseDatatype.TIME.value:
         # change format to '+%Y-%m-%dT00:00:00Z'
         if __propcode == "P19":
-            return String(value=__value, prop_nr=__propcode)
-        else:
-            __date = datetime.strptime(str(__value), '%d/%m/%Y').strftime('%Y-%m-%dT00:00:00Z')
+            return String(value=str(__value), prop_nr=__propcode)
 
+        __date = datetime.strptime(str(__value), '%d/%m/%Y').strftime('%Y-%m-%dT00:00:00Z')
         return Time(time=__date, precision=WikibaseDatePrecision.DAY, prop_nr=__propcode)
     elif __data_type == WikibaseDatatype.URL.value:
         return URL(value=__value, prop_nr=__propcode)
