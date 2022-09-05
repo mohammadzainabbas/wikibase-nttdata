44381e7d9f9ed03ac12716082755f96dae24c79b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-05 00:12:49 +0000

    Updated code

--- src/create_projects.py
+++ src/create_projects.py
@@ -40,7 +40,7 @@ def convert_to_wikibase_datatype(__value, __propcode, __data_type, __res):
     if __data_type == WikibaseDatatype.STRING.value:
         return String(value=__value, prop_nr=__propcode)
     elif (__data_type == "item") or (__data_type == WikibaseDatatype.ITEM.value):
-        __res
+        __prop_code = pd.DataFrame(__res).set_index("item").T.to_dict()[__value]
         return Item(value=__value, prop_nr=__propcode)
     elif __data_type == WikibaseDatatype.QUANTITY.value:
         return Quantity(value=__value, prop_nr=__propcode)
