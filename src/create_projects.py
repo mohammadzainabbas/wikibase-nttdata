c247ae0039cbbbe8510469aa77f63fadc284f2fc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-09-05 00:14:12 +0000

    Minor update

--- src/create_projects.py
+++ src/create_projects.py
@@ -42,7 +42,8 @@ def convert_to_wikibase_datatype(__value, __propcode, __data_type, __res):
     elif (__data_type == "item") or (__data_type == WikibaseDatatype.ITEM.value):
         prop_code = pd.DataFrame(__res).set_index("item").T.to_dict()[__value]
         if not prop_code:
-            print_error("Invalid data type for property: '{}'".format(prop_code))
+            print_error("Unable to find: '{}' item".format(__value))
+            return Item()
         else:
             return Item(value=__value, prop_nr=prop_code)
     elif __data_type == WikibaseDatatype.QUANTITY.value:
